fa2b7de79072b4f217ae8f4209c36d070ba1a32a
github_cloudlabsuser_2099@cloudlabsaiuser.com <github_cloudlabsuser_2099@cloudlabsaiuser.com>
2024-06-04 18:33:50 +0000

    Added Logic for varius Crud methods

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,7 +1,7 @@
 using CRUD_application_2.Models;
 using System.Linq;
 using System.Web.Mvc;
- 
+
 namespace CRUD_application_2.Controllers
 {
     public class UserController : Controller
@@ -11,36 +11,63 @@ namespace CRUD_application_2.Controllers
         public ActionResult Index()
         {
             // Implement the Index method here
+            return View(userlist);
         }
- 
+
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
             // Implement the details method here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(user);
         }
- 
+
         // GET: User/Create
         public ActionResult Create()
         {
-            //Implement the Create method here
+            return View();
         }
- 
+
         // POST: User/Create
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(User user)
         {
             // Implement the Create method (POST) here
+            if (ModelState.IsValid)
+            {
+                // Assuming userlist is your data store
+                userlist.Add(user);
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
         }
- 
+
         // GET: User/Edit/5
         public ActionResult Edit(int id)
         {
             // This method is responsible for displaying the view to edit an existing user with the specified ID.
             // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(user);
         }
- 
+
         // POST: User/Edit/5
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Edit(int id, User user)
         {
             // This method is responsible for handling the HTTP POST request to update an existing user with the specified ID.
@@ -48,19 +75,55 @@ namespace CRUD_application_2.Controllers
             // If successful, it redirects to the Index action to display the updated list of users.
             // If no user is found with the provided ID, it returns a HttpNotFoundResult.
             // If an error occurs during the process, it returns the Edit view to display any validation errors.
+            if (ModelState.IsValid)
+            {
+                var existingUser = userlist.FirstOrDefault(u => u.Id == id);
+
+                if (existingUser == null)
+                {
+                    return HttpNotFound();
+                }
+
+                // Assuming User has properties Name, Email, etc.
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+                // Update other properties as needed
+
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
         }
- 
+
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
             // Implement the Delete method here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(user);
         }
- 
+
         // POST: User/Delete/5
-        [HttpPost]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, FormCollection collection)
         {
             // Implement the Delete method (POST) here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            userlist.Remove(user);
+            return RedirectToAction("Index");
         }
     }
 }
